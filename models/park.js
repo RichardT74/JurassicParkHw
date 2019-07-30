1a1c60ec1445619e9f16863537c52c928f022a9b
RichardT74 <richtrist@gmail.com>
2019-07-30 18:26:10 +0100

    just started add dio

---
+++ models/park.js
@@ -0,0 +1,12 @@
+const Park = function
+	(name, ticketPrice) {
+		this.name = name;
+		this.ticketPrice = ticketPrice;
+		this.dinosaurArray = [];
+		this.dinosaurArray.push(dinosaur);
+	}
+
+
+
+
+	module.exports = Park;
